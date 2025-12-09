0d63a6ce31cae643ea31db52e9e4d6614ffd387b
Jean Flores <jeansfloresof@gmail.com>
2025-12-09 02:35:40 -0300

    add all nvim configs

---
+++ nvim/.config/nvim/lua/plugins/oldworld.lua
@@ -0,0 +1,14 @@
+return {
+  "dgox16/oldworld.nvim",
+  opts = {
+    terminal_colors = true, -- enable terminal colors
+    variant = "oled", -- default, oled, cooler
+    styles = {
+      comments = { italic = true },
+      keywords = { bold = true },
+      functions = { italic = true },
+      variables = { bold = true },
+      types = { bold = true, italic = true },
+    },
+  },
+}
