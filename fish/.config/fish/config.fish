d57b506336caeb28f1d05a145c81343558a375e3
Jean Flores <jeansfloresof@gmail.com>
2025-12-20 22:37:07 -0300

    change asdf for mise in the fish config

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,3 +1,17 @@
 starship init fish | source
 
-/usr/bin/mise activate fish | source
+set -gx PATH $PATH ~/go/bin
+
+if test -z $ASDF_DATA_DIR
+    set _asdf_shims "$HOME/.asdf/shims"
+else
+    set _asdf_shims "$ASDF_DATA_DIR/shims"
+end
+
+if not contains $_asdf_shims $PATH
+    set -gx --prepend PATH $_asdf_shims
+end
+
+set --erase _asdf_shims
+
+# /usr/bin/mise activate fish | source
