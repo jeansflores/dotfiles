6b4c5b26bcde29b1af0855ca8ce609f9a562753e
Jean Flores <jeansfloresof@gmail.com>
2025-12-26 02:12:10 -0300

    change colorscheme for tokyonight

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,17 +1,3 @@
 starship init fish | source
 
-set -gx PATH $PATH ~/go/bin
-
-if test -z $ASDF_DATA_DIR
-    set _asdf_shims "$HOME/.asdf/shims"
-else
-    set _asdf_shims "$ASDF_DATA_DIR/shims"
-end
-
-if not contains $_asdf_shims $PATH
-    set -gx --prepend PATH $_asdf_shims
-end
-
-set --erase _asdf_shims
-
-# /usr/bin/mise activate fish | source
+/usr/bin/mise activate fish | source
---
+++ nvim/.config/nvim/lua/plugins/tokyonight.lua
@@ -0,0 +1,9 @@
+return {
+  "folke/tokyonight.nvim",
+  lazy = false,
+  opts = {
+    style = "storm",
+    transparent = false,
+    terminal_colors = true,
+  },
+}
