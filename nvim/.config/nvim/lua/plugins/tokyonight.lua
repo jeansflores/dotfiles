6fa2df7a37a75ca5d9aef3f1dc1822f0ed84c2ea
Jean Flores <jeansfloresof@gmail.com>
2025-12-29 14:58:47 -0300

    update tokyonight colorscheme config

--- nvim/.config/nvim/lua/plugins/tokyonight.lua
+++ nvim/.config/nvim/lua/plugins/tokyonight.lua
@@ -3,7 +3,19 @@ return {
   lazy = false,
   opts = {
     style = "moon",
-    transparent = false,
+    transparent = true,
     terminal_colors = true,
+    styles = {
+      sidebars = "transparent",
+      floats = "transparent",
+    },
+    on_highlights = function(hl)
+      hl.NormalFloat = { bg = "none" }
+      hl.FloatBorder = { bg = "none" }
+      hl.TelescopeNormal = { bg = "none" }
+      hl.TelescopeBorder = { bg = "none" }
+      hl.NeoTreeNormal = { bg = "none" }
+      hl.NeoTreeNormalNC = { bg = "none" }
+    end,
   },
 }
