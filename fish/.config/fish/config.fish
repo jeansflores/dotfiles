329e589200a86511616f4a58a8922a4b3f1508f1
Jean Flores <jeansfloresof@gmail.com>
2026-03-19 00:32:15 -0300

    change mise bin to fish

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,3 +1,3 @@
-# /usr/sbin/mise activate fish | source # Fedora
-~/.local/bin/mise activate fish | source # Ubuntu
+/usr/sbin/mise activate fish | source # Fedora
+# ~/.local/bin/mise activate fish | source # Ubuntu
 fish_add_path $HOME/.local/bin
