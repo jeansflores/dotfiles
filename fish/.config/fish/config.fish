ea9f4ddc1a0fe24aae540177979eac5b84514cc2
Jean Flores <jeansfloresof@gmail.com>
2026-04-25 23:29:55 -0300

    update alacritty and fish configs

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,3 +1,7 @@
-/usr/bin/mise activate fish | source
+~/.local/bin/mise activate fish | source
 
 fish_add_path $HOME/.local/bin
+
+# bun
+set --export BUN_INSTALL "$HOME/.bun"
+set --export PATH $BUN_INSTALL/bin $PATH
