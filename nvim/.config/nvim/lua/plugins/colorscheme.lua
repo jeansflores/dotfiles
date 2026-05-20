9588ffdcd9bd19594fab14d536cd2b456e28d586
Jean Flores <jeansfloresof@gmail.com>
2026-05-20 11:18:40 -0300

    update nvim tokyonight with transparent background

--- nvim/.config/nvim/lua/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -1,6 +1,19 @@
 return {
-  "LazyVim/LazyVim",
-  opts = {
-    colorscheme = "tokyonight-moon",
+  {
+    "folke/tokyonight.nvim",
+    opts = {
+      style = "moon",
+      transparent = true,
+      styles = {
+        sidebars = "transparent",
+        floats = "transparent",
+      },
+    },
+  },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "tokyonight-moon",
+    },
   },
 }
