b943f895d1d478cb7642ef6758897531f49a5e3e
Jean Flores <jeansfloresof@gmail.com>
2026-06-14 01:44:27 -0300

    remove old configs

--- fish/.config/fish/config.fish
+++
@@ -1,6 +0,0 @@
-source /usr/share/cachyos-fish-config/cachyos-config.fish
-
-function fish_greeting
-end
-
-mise activate fish | source
--- polybar/.config/polybar/launch.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-killall -q polybar
-while pgrep -u $UID -x polybar > /dev/null; do sleep 0.5; done
-
-# lança uma barra por monitor ativo (com resolução atribuída)
-for m in $(xrandr --query | grep " connected" | grep -oP "^\S+"); do
-    MONITOR=$m polybar --reload main &
-done
